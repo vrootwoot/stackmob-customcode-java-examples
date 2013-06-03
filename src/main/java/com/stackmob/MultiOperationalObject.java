6b8f41e2dfa4ed5bfd8a388015951c390d1dd189
unknown <domriordan@gmail.com>
2013-06-03 13:47:50 +0100

    multi

--- src/main/java/com/stackmob/MultiOperationalObject.java
+++ src/main/java/com/stackmob/MultiOperationalObject.java
@@ -89,16 +89,16 @@ public class MultiOperationalObject implements CustomCodeMethod {
             // loop through each column within array == table column
             for (int l=0; create_list[i][k].length(); l++)
             {
-                if (create_list[i][k][l][0].equals('list')) {
-                    feedback.put(create_list[i][k][l][1], new SMList(List.parseList(create_list[i][k][l][2])));
-                } else if (create_list[i][k][l][0].equals('map')) {
-                    feedback.put(create_list[i][k][l][1], new SMMap(Map.parseMap(create_list[i][k][l][2])));
-                } else if (create_list[i][k][l][0].equals('string')) {
-                    feedback.put(create_list[i][k][l][1], new SMString(create_list[i][k][l][2]));    
+                if (create_list[i][k][l][0].equals("list")) {
+                    //feedback.put(create_list[i][k][l][1], new SMList(List.parseList(create_list[i][k][l][2])));
+                } else if (create_list[i][k][l][0].equals("map")) {
+                    //feedback.put(create_list[i][k][l][1], new SMMap(Map.parseMap(create_list[i][k][l][2])));
+                } else if (create_list[i][k][l][0].equals("string")) {
+                    //feedback.put(create_list[i][k][l][1], new SMString(create_list[i][k][l][2]));    
                 } else if (create_list[i][k][l][0].equals('long')) {
-                    feedback.put(create_list[i][k][l][1], new SMInt(Long.parseLong(create_list[i][k][l][2])));
+                    //feedback.put(create_list[i][k][l][1], new SMInt(Long.parseLong(create_list[i][k][l][2])));
                 } else if (create_list[i][k][l][0].equals('double')) {
-                    feedback.put(create_list[i][k][l][1], new SMDouble(Double.parseLong(create_list[i][k][l][2])));
+                    //feedback.put(create_list[i][k][l][1], new SMDouble(Double.parseLong(create_list[i][k][l][2])));
                 }
             }
             try {
