195b366a8d5f0ede197bc68e9b6442e454e4f954
unknown <domriordan@gmail.com>
2013-06-03 17:16:06 +0100

    multi

--- src/main/java/com/stackmob/MultiOperationalObject.java
+++ src/main/java/com/stackmob/MultiOperationalObject.java
@@ -9,7 +9,6 @@ import com.stackmob.core.rest.ResponseToProcess;
 import com.stackmob.example.Util;
 import com.stackmob.sdkapi.SDKServiceProvider;
 import com.stackmob.sdkapi.*;
-import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import org.json.JSONArray;
