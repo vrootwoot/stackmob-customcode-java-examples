ff7255f7ecaadf9801aab27e1151b1103f888938
Bérenger <berenger.laurent@gmail.com>
2013-06-06 00:47:00 +0200

    qsd
    Signed-off-by: Bérenger <berenger.laurent@gmail.com>

--- src/main/java/com/stackmob/example/CRUD/GlobalOperations.java
+++ src/main/java/com/stackmob/example/CRUD/GlobalOperations.java
@@ -12,15 +12,6 @@ import com.stackmob.sdkapi.*;
 
 import com.google.gson.*;
 
-//import org.json.simple.parser.JSONParser;
-//import org.json.simple.parser.ParseException;
-//import org.json.simple.JSONObject;
-//import org.json.simple.JSONArray;
-//import org.json.simple.JSONValue;
-
-//import org.json.parser.*;
-//import org.json.*;
- 
 
 import java.net.HttpURLConnection;
 import java.util.*;
@@ -29,64 +20,9 @@ import java.util.Map;
 
 import java.util.ArrayList;
 
-/**
- * This example will show a user how to write a custom code method
- * with two parameters that updates the specified object in their schema
- * when given a unique ID and a `year` field on which to update.
- */
-
 
 
 public class GlobalOperations implements CustomCodeMethod {
-  
-    static class TableFields {
-        public String data_type;
-        public String column_name;
-        public String data_value;
-        public String table_name;
-    }
-    
-    static class TableEntry {
-        
-       public TableFields[] table_fields;
-       
-       public TableEntry(TableFields[] table_fields) {
-           this.table_fields = table_fields;
-       }
-       
-        public TableFields[] getFields() {
-            return this.table_fields;
-        }  
-        
-        
-    }
-    
-    static class TableOperation {
-        public TableEntry[] table_entry;
-        
-        public TableOperation(TableEntry[] table_entry) {
-            this.table_entry = table_entry;
-        }
-        
-        public TableEntry[] getEntries() {
-            return this.table_entry;
-        }        
-    }
-    
-   static class TableOperations {
-       
-        public TableOperation[] operation;
-
-        public TableOperations(TableOperation[] operation) {
-          this.operation = operation;
-        }
-        
-        public TableOperation getOperation(Integer op) {
-            return this.operation[op];
-        }
-        
-    }   
-    
 
   @Override
   public String getMethodName() {
@@ -115,14 +51,8 @@ public class GlobalOperations implements CustomCodeMethod {
   @Override
   public ResponseToProcess execute(ProcessedAPIRequest request, SDKServiceProvider serviceProvider) {
       logger = serviceProvider.getLoggerService(GlobalOperations.class);
-      logger.debug(request.getParams().get("object_operations"));
-    
 
-  
-    
-    Gson gson = new Gson();  
-    JsonParser parser = new JsonParser();
-    logger.debug(parser);
+  logger.debug("log anything");
     return new ResponseToProcess(HttpURLConnection.HTTP_OK, feedback);
 
   }
