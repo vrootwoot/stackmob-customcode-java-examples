cd38023581fd120da3dd2ee83ee885fcb738dedc
unknown <domriordan@gmail.com>
2013-06-05 10:21:56 +0100

    multi

--- src/main/java/com/stackmob/example/CRUD/MultiOperationalObject.java
+++ src/main/java/com/stackmob/example/CRUD/MultiOperationalObject.java
@@ -93,7 +93,7 @@ public class MultiOperationalObject implements CustomCodeMethod {
       //Object obj = parser.parse(request.getParams().get("object_operations"));
       //JSONObject jsonObject = (JSONObject) obj;
       
-      JSONObject jsonObject = (JSONObject) parser.parse(request.getParams().get("object_operations"));  
+      org.json.simple.JSONObject jsonObject = (org.json.simple.JSONObject) parser.parse(request.getParams().get("object_operations"));  
 
       
       // Fetch the values passed in by the user from the body of JSON
